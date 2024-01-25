8ba1a8d2b92bb641bef1f14e3501c2d267879fc6
45183 <hsb9743@naver.com>
2024-01-25 09:41:27 +0900

    검색 기능

--- backend/src/models/Community.js
+++ backend/src/models/Community.js
@@ -28,6 +28,22 @@ const communitySchema = mongoose.Schema({
     }
 })
 
+// Mongo DB Partial Text Search
+// 게시글 작성자 검색하려면 그냥 writer로는 검색되지 않음 -> 수정 필요
+communitySchema.index({
+    // 검색할 내용
+    title: 'text',
+    contents: 'text',
+    writer: 'text'
+}, {
+    weights: { 
+        // 중요도 (클수록 중요도 높음)
+        title: 5,
+        contents: 1,
+        writer: 3
+    }
+});
+
 const Community = mongoose.model("Community", communitySchema);
 
 module.exports = Community;
